dd354fc7b5292e987ab45b6f3ac8adcc406cf7df
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-12-25 15:46:18 +0300

    initialized project and ef core with codeFirst method and created database

---
+++ eCommerce/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Category>Categories { get; set; }
+
+    }
+}
---
+++ eCommerce/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    public class Category
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
