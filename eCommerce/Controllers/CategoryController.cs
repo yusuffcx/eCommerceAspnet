6d9a0b36c2300bb189afc0d8a0c22aa0c337dcb8
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-12-27 13:03:34 +0300

    added Category Create and Read operations with ef core

---
+++ eCommerce/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using eCommerce.Contexts;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public List<Category>categories { get; set; }
+        public IActionResult Index()
+        {
+            categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
--- eCommerce/Models/Category.cs
+++ eCommerce/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace eCommerce.Models
 {
@@ -7,7 +8,10 @@ namespace eCommerce.Models
         
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+
         public String Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
     }
