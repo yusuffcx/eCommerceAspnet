ce7b9ced4c02d8371cac1286fdb3ec82b42806d1
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2025-03-15 12:46:09 +0300

    finished order summary page

--- eCommerce/obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
+++ eCommerce/obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ed82617a4ccd3e3caacbee0e710d1427ba12ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006787cfab5a066b76b7f145b14e5a47114a5b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
